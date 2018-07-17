ac5e8689899ce80c0b649a9444caa454cf3b9a29
Cristian Quiroz <cquirozp@alumnos.upacifico.cl>
2018-07-17 18:25:05 -0400

    Imoplementa correciones desktop

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
-	console.log('this is test');
+	$('.c-header__mobile-open').click(function(e){
+		$('.c-header__navOpen').addClass('c-header__navOpen--open');
+		e.preventDefault();
+	});
+
+	$('.close-nav').click(function (e) {
+		$('.c-header__navOpen').removeClass('c-header__navOpen--open');
+		e.preventDefault();
+	});	
 });
 
