e17217e3aa65aa75ab2d1195e505a3f5b7a4a4df
Cristian Quiroz <cquirozp@alumnos.upacifico.cl>
2018-07-30 20:11:23 -0400

    Implementa modal y mapa con api

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(e) {
+	var win = $(window).width();
+
 	$('.c-header__mobile-open').click(function(e){
 		$('.c-header__navOpen').addClass('c-header__navOpen--open');
 		e.preventDefault();
@@ -12,5 +14,40 @@ $(document).ready(function(e) {
 	$('.c-blog__articles').masonry({
 		itemSelector: '.c-blog__article',
 		columnWidth: '.grid-sizer'
+	});
+
+	$('.c-modalCupon__overlay').click(function(e){
+		$('.c-modalCupon').hide();
+
+		e.preventDefault();
+	});
+
+	$('.buscadorResultados__button').click(function (e) {
+		$('.c-modalCupon').show();
+
+		e.preventDefault();
 	});	
+
+	if(win < 992) {
+		$('.c-modalMobile__btn').click(function (e) {
+			$('.c-modalMobile__firstStep').removeClass('c-modalMobile__firstStep--active');
+			$('.c-modalMobile__secondStep').addClass('c-modalMobile__secondStep--active');
+
+			e.preventDefault();
+		});
+
+		$('.buscadorResultados__button').click(function (e) {
+			$('.c-modalMobile').show();
+
+			e.preventDefault();
+		});			
+
+		$('.c-modalMobile__btn-imprimir').click(function (e) {
+			$('.c-modalMobile__firstStep').addClass('c-modalMobile__firstStep--active');
+			$('.c-modalMobile__secondStep').removeClass('c-modalMobile__secondStep--active');			
+			$('.c-modalMobile').hide();
+
+			e.preventDefault();
+		});
+	}
 });
